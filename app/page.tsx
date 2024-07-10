07fd6a3f0c03dd381c4d9d75aeff74c4b3321695
ali ounassi <aliounassi@gmail.com>
2024-07-10 21:52:56 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,145 @@
 import Image from "next/image";
+import SearchIcon from "@mui/icons-material/Search";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import CategoryIcon from "@mui/icons-material/Category";
+import LandslideIcon from "@mui/icons-material/Landslide";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex poppins">
+      <SideBar />
+      <ContentArea />
+    </div>
+  );
+}
+
+function SideBar() {
+  return <div className="h-screen w-[260px]">This is a sidebar</div>;
+}
+
+function ContentArea() {
+  return (
+    <div className="h-screen w-full bg-slate-100 border p-3">
+      <TopBar />
+      <StatsBar />
+      <AllCategories />
+    </div>
+  );
+
+  function TopBar() {
+    return (
+      <div className="bg-white w-full p-[11px] rounded-lg px-6 flex justify-between items-center">
+        <DashboardText />
+        <SearchBar />
+        <div className="flex gap-4 items-center">
+          <DarkMode />
+          <ProfileAccount />
         </div>
       </div>
+    );
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+    function DashboardText() {
+      return (
+        <div className="  ">
+          <div className="flex flex-col ">
+            <span className="font-semibold">Welcome Back, Ali</span>
+            <span className="text-slate-400 text-[11px] font-light">
+              This is a sbub message
+            </span>
+          </div>
+        </div>
+      );
+    }
+
+    function SearchBar() {
+      return (
+        <div className="bg-slate-100 w-1/3   p-[8px] flex gap-1 justify-center   items-center rounded-md">
+          <SearchIcon fontSize="small" className="text-slate-500" />
+          <span className="text-slate-500 text-sm">Search</span>
+        </div>
+      );
+    }
+
+    function DarkMode() {
+      return (
+        <div>
+          <LightModeIcon className="text-orange-600" fontSize="small" />
+        </div>
+      );
+    }
+
+    function ProfileAccount() {
+      return (
+        <div className=" flex gap-3 items-center  ">
+          <div className="w-[36px] h-[37px] bg-slate-100 rounded-full"></div>
+        </div>
+      );
+    }
+  }
+
+  function StatsBar() {
+    return (
+      <div className="grid grid-cols-3 gap-4   mt-4   rounded-lg">
+        <CategoriesCard />
+        <CategoriesCard />
+        <CategoriesCard />
       </div>
+    );
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+    function CategoriesCard() {
+      return (
+        <div className="flex gap-4 items-center p-3    bg-white rounded-lg">
+          <div className="w-[45px] h-[45px] bg-pink-100 rounded-full flex items-center justify-center">
+            <CategoryIcon className="text-pink-400" />
+          </div>
+          <div className="flex flex-col">
+            <span className="font-bold text-xl">12</span>
+            <span className="  text-sm font-light text-slate-400 ">
+              Categories
             </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          </div>
+        </div>
+      );
+    }
+  }
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+  function AllCategories() {
+    return (
+      <div className=" bg-white  w-full p-8 rounded-lg mt-4">
+        <span className="text-lg flex gap-2 justify-between items-center">
+          <div className="">
+            <span className="font-bold">All Categories</span>
+          </div>
+          <button className="bg-pink-500 text-white text-[12px] px-2 py-[2px] rounded-md">
+            + New Category
+          </button>
+        </span>
+        <div className="flex flex-wrap gap-4 mt-7 ">
+          <SingleCategory />
+          <SingleCategory />
+          <SingleCategory />
+          <SingleCategory />
+          <SingleCategory />
+          <SingleCategory />
+        </div>
       </div>
-    </main>
-  );
+    );
+
+    function SingleCategory() {
+      return (
+        <div className="w-[200px] border border-pink-100 rounded-md p-5 flex gap-2 justify-center flex-col items-center    ">
+          <div className="w-[70px] h-[70px] bg-pink-100 rounded-full flex items-center justify-center">
+            <LandslideIcon className="text-[30px] text-pink-400" />
+          </div>
+
+          <div className="flex flex-col items-center justify-center">
+            <span className="font-semibold text-lg">Buttons</span>
+            <span className="text-[12px] text-slate-400 text-center">
+              10 Components
+            </span>
+          </div>
+        </div>
+      );
+    }
+  }
 }
